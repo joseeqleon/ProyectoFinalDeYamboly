412646fcc8f55886fdb746c4210e6ca9fcef2d94
Jose <Jose@DESKTOP-OEDD3GV>
2025-07-09 05:34:49 -0500

    Form de Mantenedor Empresa

---
+++ MantenedorEmpresa.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace ProyectoYambolyFinal
+{
+    partial class MantenedorEmpresa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbUbigeo = new System.Windows.Forms.ComboBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgvEmpresas = new System.Windows.Forms.DataGridView();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.txtRUC = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpresas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbUbigeo
+            // 
+            this.cmbUbigeo.FormattingEnabled = true;
+            this.cmbUbigeo.Location = new System.Drawing.Point(65, 108);
+            this.cmbUbigeo.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbUbigeo.Name = "cmbUbigeo";
+            this.cmbUbigeo.Size = new System.Drawing.Size(134, 21);
+            this.cmbUbigeo.TabIndex = 30;
+            this.cmbUbigeo.SelectedIndexChanged += new System.EventHandler(this.cmbUbigeo_SelectedIndexChanged);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(286, 148);
+            this.btnMostrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(69, 19);
+            this.btnMostrar.TabIndex = 29;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(68, 5);
+            this.txtID.Margin = new System.Windows.Forms.Padding(2);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(63, 20);
+            this.txtID.TabIndex = 28;
+            this.txtID.TextChanged += new System.EventHandler(this.txtID_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 5);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "ID-Empresa";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dgvEmpresas
+            // 
+            this.dgvEmpresas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmpresas.Location = new System.Drawing.Point(6, 182);
+            this.dgvEmpresas.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvEmpresas.Name = "dgvEmpresas";
+            this.dgvEmpresas.RowHeadersWidth = 51;
+            this.dgvEmpresas.RowTemplate.Height = 24;
+            this.dgvEmpresas.Size = new System.Drawing.Size(349, 220);
+            this.dgvEmpresas.TabIndex = 26;
+            this.dgvEmpresas.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEmpresas_CellContentClick);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(134, 148);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(64, 19);
+            this.btnEliminar.TabIndex = 25;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(65, 148);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(64, 19);
+            this.btnModificar.TabIndex = 24;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(4, 148);
+            this.btnNuevo.Margin = new System.Windows.Forms.Padding(2);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(56, 19);
+            this.btnNuevo.TabIndex = 23;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // txtRUC
+            // 
+            this.txtRUC.Location = new System.Drawing.Point(60, 72);
+            this.txtRUC.Margin = new System.Windows.Forms.Padding(2);
+            this.txtRUC.Name = "txtRUC";
+            this.txtRUC.Size = new System.Drawing.Size(152, 20);
+            this.txtRUC.TabIndex = 22;
+            this.txtRUC.TextChanged += new System.EventHandler(this.txtRUC_TextChanged);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(60, 38);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(295, 20);
+            this.txtNombre.TabIndex = 21;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 110);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "UBIGEO: ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 72);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "RUC:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "NOMBRE:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MantenedorEmpresa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 404);
+            this.Controls.Add(this.cmbUbigeo);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dgvEmpresas);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.txtRUC);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MantenedorEmpresa";
+            this.Text = "MantenedorEmpresa";
+            this.Load += new System.EventHandler(this.MantenedorEmpresa_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpresas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbUbigeo;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgvEmpresas;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.TextBox txtRUC;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ MantenedorEmpresa.cs
@@ -0,0 +1,173 @@
+﻿using CapaEntidad;
+using CapaLogica;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoYambolyFinal
+{
+    public partial class MantenedorEmpresa : Form
+    {
+        public MantenedorEmpresa()
+        {
+            InitializeComponent();
+            Listar();
+        }
+        private void Listar()
+        {
+            dgvEmpresas.DataSource = logEmpresa.Instancia.ListarEmpresa();
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtRUC_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbUbigeo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void CargarUbigeo()
+        {
+            cmbUbigeo.DataSource = logUbigeo.Instancia.ListarUbigeo();
+            cmbUbigeo.DisplayMember = "ToString";    // Departamento / Provincia / Distrito
+            cmbUbigeo.ValueMember = "UbigeoID";
+        }
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text.Trim();
+            string ruc = txtRUC.Text.Trim();
+            entUbigeo ubigeoSeleccionado = cmbUbigeo.SelectedItem as entUbigeo;
+
+            if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(ruc) || ubigeoSeleccionado == null)
+            {
+                MessageBox.Show("Debe completar todos los campos.");
+                return;
+            }
+
+            // Verificar si el RUC ya existe (como antes)
+            if (logEmpresa.Instancia.ExisteRUC(ruc))
+            {
+                MessageBox.Show("Este RUC ya está registrado.");
+                return;
+            }
+
+            var emp = new entEmpresa
+            {
+                Nombre = nombre,
+                RUC = ruc,
+                Ubigeo = ubigeoSeleccionado,
+                Estado = true
+            };
+
+            logEmpresa.Instancia.InsertarEmpresa(emp);
+            MessageBox.Show("Empresa registrada correctamente.");
+            Listar();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtID.Text, out int id))
+            {
+                MessageBox.Show("Seleccione una empresa válida.");
+                return;
+            }
+
+            string nombre = txtNombre.Text.Trim();
+            string ruc = txtRUC.Text.Trim();
+            entUbigeo ubigeoSeleccionado = cmbUbigeo.SelectedItem as entUbigeo;
+
+            if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(ruc) || ubigeoSeleccionado == null)
+            {
+                MessageBox.Show("Complete todos los campos.");
+                return;
+            }
+
+            var emp = new entEmpresa
+            {
+                EmpresaID = id,
+                Nombre = nombre,
+                RUC = ruc,
+                Ubigeo = ubigeoSeleccionado,
+                Estado = true
+            };
+
+            logEmpresa.Instancia.EditarEmpresa(emp);
+            MessageBox.Show("Empresa modificada correctamente.");
+            Listar();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int idSeleccionado = Convert.ToInt32(txtID.Text);
+            DialogResult result = MessageBox.Show("¿Está seguro de eliminar esta empresa?", "Confirmar", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                bool eliminado = logEmpresa.Instancia.EliminarEmpresa(idSeleccionado);
+                if (eliminado)
+                {
+                    MessageBox.Show("Empresa eliminada correctamente.");
+                    Listar();
+                }
+                else
+                {
+                    MessageBox.Show("Error al eliminar la empresa.");
+                }
+            }
+        }
+        private void MantenedorEmpresa_Load(object sender, EventArgs e)
+        {
+            CargarUbigeo();  // ✅ Cargar combo de Ubigeo
+            Listar();
+        }
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void dgvEmpresas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void MantenedorEmpresa_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
