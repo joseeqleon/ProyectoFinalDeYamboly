ba688ccec5c10288af77df5f6696c6f2dd4b1359
Jose <Jose@DESKTOP-OEDD3GV>
2025-07-09 04:44:50 -0500

    Form de Pago

---
+++ FormPago.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace ProyectoYambolyFinal
+{
+    partial class FormPago
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtEmpresa = new System.Windows.Forms.TextBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.dgvComprobantes = new System.Windows.Forms.DataGridView();
+            this.dtpFechaEntrega = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbMetodoPago = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbPedidoID = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvComprobantes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtEmpresa);
+            this.groupBox1.Controls.Add(this.btnMostrar);
+            this.groupBox1.Controls.Add(this.btnRegistrar);
+            this.groupBox1.Controls.Add(this.dgvComprobantes);
+            this.groupBox1.Controls.Add(this.dtpFechaEntrega);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.cmbMetodoPago);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cmbPedidoID);
+            this.groupBox1.Location = new System.Drawing.Point(1, 1);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(489, 466);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Realiza Pago";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // txtEmpresa
+            // 
+            this.txtEmpresa.Location = new System.Drawing.Point(59, 53);
+            this.txtEmpresa.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmpresa.Name = "txtEmpresa";
+            this.txtEmpresa.Size = new System.Drawing.Size(188, 20);
+            this.txtEmpresa.TabIndex = 25;
+            this.txtEmpresa.TextChanged += new System.EventHandler(this.txtEmpresa_TextChanged);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(93, 136);
+            this.btnMostrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(62, 19);
+            this.btnMostrar.TabIndex = 24;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(9, 136);
+            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(70, 19);
+            this.btnRegistrar.TabIndex = 23;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // dgvComprobantes
+            // 
+            this.dgvComprobantes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvComprobantes.Location = new System.Drawing.Point(9, 159);
+            this.dgvComprobantes.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvComprobantes.Name = "dgvComprobantes";
+            this.dgvComprobantes.RowHeadersWidth = 51;
+            this.dgvComprobantes.RowTemplate.Height = 24;
+            this.dgvComprobantes.Size = new System.Drawing.Size(476, 288);
+            this.dgvComprobantes.TabIndex = 15;
+            this.dgvComprobantes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvComprobantes_CellContentClick);
+            // 
+            // dtpFechaEntrega
+            // 
+            this.dtpFechaEntrega.Location = new System.Drawing.Point(96, 110);
+            this.dtpFechaEntrega.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpFechaEntrega.Name = "dtpFechaEntrega";
+            this.dtpFechaEntrega.Size = new System.Drawing.Size(206, 20);
+            this.dtpFechaEntrega.TabIndex = 6;
+            this.dtpFechaEntrega.ValueChanged += new System.EventHandler(this.dtpFechaEntrega_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 110);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Fecha de entrega";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 53);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Empresa";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // cmbMetodoPago
+            // 
+            this.cmbMetodoPago.FormattingEnabled = true;
+            this.cmbMetodoPago.Location = new System.Drawing.Point(93, 78);
+            this.cmbMetodoPago.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbMetodoPago.Name = "cmbMetodoPago";
+            this.cmbMetodoPago.Size = new System.Drawing.Size(187, 21);
+            this.cmbMetodoPago.TabIndex = 3;
+            this.cmbMetodoPago.SelectedIndexChanged += new System.EventHandler(this.cmbMetodoPago_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IDPedido";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Metodo de pago";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // cmbPedidoID
+            // 
+            this.cmbPedidoID.FormattingEnabled = true;
+            this.cmbPedidoID.Location = new System.Drawing.Point(59, 24);
+            this.cmbPedidoID.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbPedidoID.Name = "cmbPedidoID";
+            this.cmbPedidoID.Size = new System.Drawing.Size(183, 21);
+            this.cmbPedidoID.TabIndex = 2;
+            this.cmbPedidoID.SelectedIndexChanged += new System.EventHandler(this.cmbPedidoID_SelectedIndexChanged);
+            // 
+            // FormPago
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 452);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormPago";
+            this.Text = "FormPago";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvComprobantes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtEmpresa;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.DataGridView dgvComprobantes;
+        private System.Windows.Forms.DateTimePicker dtpFechaEntrega;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbMetodoPago;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbPedidoID;
+    }
+}
---
+++ FormPago.cs
@@ -0,0 +1,124 @@
+﻿using CapaEntidad;
+using CapaLogica;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace ProyectoYambolyFinal
+{
+    public partial class FormPago : Form
+    {
+        public FormPago()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbPedidoID_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbPedidoID.SelectedItem is entPedido pedido)
+            {
+                txtEmpresa.Text = pedido.EmpresaNombre;
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEmpresa_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbMetodoPago_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtpFechaEntrega_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            if (dtpFechaEntrega.Value.Date < DateTime.Today)
+            {
+                MessageBox.Show("La fecha no puede ser anterior a hoy.");
+                return;
+            }
+
+            entComprobantePago comp = new entComprobantePago
+            {
+                PedidoID = Convert.ToInt32(cmbPedidoID.SelectedValue),
+                MetodoPagoID = Convert.ToInt32(cmbMetodoPago.SelectedValue),
+                FechaEntrega = dtpFechaEntrega.Value.Date
+            };
+
+            bool ok = logComprobantePago.Instancia.RegistrarComprobante(comp);
+            MessageBox.Show(ok ? "Registrado correctamente" : "Error al registrar");
+        }
+        private void FormPago_Load(object sender, EventArgs e)
+        {
+            // Cargar combo de pedidos
+            cmbPedidoID.DataSource = logPedido.Instancia.ListarPedidos();
+            cmbPedidoID.DisplayMember = "PedidoID";
+            cmbPedidoID.ValueMember = "PedidoID";
+
+            // Cargar combo de métodos de pago
+            cmbMetodoPago.DataSource = logMetodoPago.Instancia.ListarMetodoPago();
+            cmbMetodoPago.DisplayMember = "Nombre";
+            cmbMetodoPago.ValueMember = "MetodoPagoID";
+
+            // Mostrar empresa relacionada al pedido
+            cmbPedidoID.SelectedIndexChanged += (s, ev) =>
+            {
+                if (cmbPedidoID.SelectedValue is int pedidoID)
+                {
+                    var pedido = logPedido.Instancia.ListarPedidos().FirstOrDefault(p => p.PedidoID == pedidoID);
+                    if (pedido != null)
+                    {
+                        var empresa = logEmpresa.Instancia.ListarEmpresa().FirstOrDefault(emp => emp.EmpresaID == pedido.EmpresaID);
+                        txtEmpresa.Text = empresa?.Nombre ?? "";
+                    }
+                }
+            };
+        }
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            dgvComprobantes.DataSource = logComprobantePago.Instancia.ListarComprobantes();
+        }
+
+        private void dgvComprobantes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
