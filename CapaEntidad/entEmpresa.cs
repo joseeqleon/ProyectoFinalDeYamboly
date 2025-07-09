c6b0568d95f683f1c23be74db83dcdcf68b277a2
Jose <Jose@DESKTOP-OEDD3GV>
2025-07-09 02:53:28 -0500

    Agrega entEmpresa

---
+++ CapaEntidad/entEmpresa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entEmpresa
+    {
+        public int EmpresaID { get; set; }
+        public string Nombre { get; set; }
+        public string RUC { get; set; }
+        public entUbigeo Ubigeo { get; set; }  // objeto relacionado
+        public bool Estado { get; set; }
+    }
+}
