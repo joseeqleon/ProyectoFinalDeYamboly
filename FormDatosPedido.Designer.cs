122a7db88e213c0903c229ca7b604e75ac28eab2
Jose <Jose@DESKTOP-OEDD3GV>
2025-07-09 04:23:08 -0500

    Form de Datos Pedido

---
+++ FormDatosPedido.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace ProyectoYambolyFinal
+{
+    partial class FormDatosPedido
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnFiltrar = new System.Windows.Forms.Button();
+            this.btnCancelarPedido = new System.Windows.Forms.Button();
+            this.btnRegistrarPedido = new System.Windows.Forms.Button();
+            this.dtpFiltroFecha = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbEmpresa = new System.Windows.Forms.ComboBox();
+            this.txtPedidoID = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbProducto = new System.Windows.Forms.ComboBox();
+            this.cmbPresentacion = new System.Windows.Forms.ComboBox();
+            this.dtpFechaPedido = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvPedidos = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPedidos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnMostrar);
+            this.groupBox1.Controls.Add(this.btnFiltrar);
+            this.groupBox1.Controls.Add(this.btnCancelarPedido);
+            this.groupBox1.Controls.Add(this.btnRegistrarPedido);
+            this.groupBox1.Controls.Add(this.dtpFiltroFecha);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.cmbEmpresa);
+            this.groupBox1.Controls.Add(this.txtPedidoID);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtCantidad);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cmbProducto);
+            this.groupBox1.Controls.Add(this.cmbPresentacion);
+            this.groupBox1.Controls.Add(this.dtpFechaPedido);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(11, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(574, 217);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Menu Pedido";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(498, 193);
+            this.btnMostrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(71, 19);
+            this.btnMostrar.TabIndex = 23;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnFiltrar
+            // 
+            this.btnFiltrar.Location = new System.Drawing.Point(404, 75);
+            this.btnFiltrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnFiltrar.Name = "btnFiltrar";
+            this.btnFiltrar.Size = new System.Drawing.Size(56, 19);
+            this.btnFiltrar.TabIndex = 22;
+            this.btnFiltrar.Text = "Filtrar";
+            this.btnFiltrar.UseVisualStyleBackColor = true;
+            this.btnFiltrar.Click += new System.EventHandler(this.btnFiltrar_Click);
+            // 
+            // btnCancelarPedido
+            // 
+            this.btnCancelarPedido.Location = new System.Drawing.Point(74, 193);
+            this.btnCancelarPedido.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCancelarPedido.Name = "btnCancelarPedido";
+            this.btnCancelarPedido.Size = new System.Drawing.Size(68, 19);
+            this.btnCancelarPedido.TabIndex = 21;
+            this.btnCancelarPedido.Text = "Cancelar";
+            this.btnCancelarPedido.UseVisualStyleBackColor = true;
+            this.btnCancelarPedido.Click += new System.EventHandler(this.btnCancelarPedido_Click);
+            // 
+            // btnRegistrarPedido
+            // 
+            this.btnRegistrarPedido.Location = new System.Drawing.Point(4, 193);
+            this.btnRegistrarPedido.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRegistrarPedido.Name = "btnRegistrarPedido";
+            this.btnRegistrarPedido.Size = new System.Drawing.Size(64, 19);
+            this.btnRegistrarPedido.TabIndex = 11;
+            this.btnRegistrarPedido.Text = "Registrar";
+            this.btnRegistrarPedido.UseVisualStyleBackColor = true;
+            this.btnRegistrarPedido.Click += new System.EventHandler(this.btnRegistrarPedido_Click);
+            // 
+            // dtpFiltroFecha
+            // 
+            this.dtpFiltroFecha.Location = new System.Drawing.Point(275, 53);
+            this.dtpFiltroFecha.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpFiltroFecha.Name = "dtpFiltroFecha";
+            this.dtpFiltroFecha.Size = new System.Drawing.Size(186, 20);
+            this.dtpFiltroFecha.TabIndex = 20;
+            this.dtpFiltroFecha.ValueChanged += new System.EventHandler(this.dtpFiltroFecha_ValueChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(273, 37);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Fecha filtro";
+            // 
+            // cmbEmpresa
+            // 
+            this.cmbEmpresa.DisplayMember = "Marca";
+            this.cmbEmpresa.FormattingEnabled = true;
+            this.cmbEmpresa.Location = new System.Drawing.Point(74, 46);
+            this.cmbEmpresa.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbEmpresa.Name = "cmbEmpresa";
+            this.cmbEmpresa.Size = new System.Drawing.Size(161, 21);
+            this.cmbEmpresa.TabIndex = 17;
+            this.cmbEmpresa.ValueMember = "Marca";
+            this.cmbEmpresa.SelectedIndexChanged += new System.EventHandler(this.cmbEmpresa_SelectedIndexChanged);
+            // 
+            // txtPedidoID
+            // 
+            this.txtPedidoID.Location = new System.Drawing.Point(74, 22);
+            this.txtPedidoID.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPedidoID.Name = "txtPedidoID";
+            this.txtPedidoID.Size = new System.Drawing.Size(80, 20);
+            this.txtPedidoID.TabIndex = 16;
+            this.txtPedidoID.TextChanged += new System.EventHandler(this.txtPedidoID_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 27);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "ID-Pedido";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(74, 141);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(80, 20);
+            this.txtCantidad.TabIndex = 14;
+            this.txtCantidad.TextChanged += new System.EventHandler(this.txtCantidad_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 143);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Cantidad";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // cmbProducto
+            // 
+            this.cmbProducto.DisplayMember = "Marca";
+            this.cmbProducto.FormattingEnabled = true;
+            this.cmbProducto.Location = new System.Drawing.Point(74, 78);
+            this.cmbProducto.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbProducto.Name = "cmbProducto";
+            this.cmbProducto.Size = new System.Drawing.Size(161, 21);
+            this.cmbProducto.TabIndex = 12;
+            this.cmbProducto.ValueMember = "Marca";
+            this.cmbProducto.SelectedIndexChanged += new System.EventHandler(this.cmbProducto_SelectedIndexChanged);
+            // 
+            // cmbPresentacion
+            // 
+            this.cmbPresentacion.DisplayMember = "Nombre";
+            this.cmbPresentacion.FormattingEnabled = true;
+            this.cmbPresentacion.Location = new System.Drawing.Point(74, 109);
+            this.cmbPresentacion.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbPresentacion.Name = "cmbPresentacion";
+            this.cmbPresentacion.Size = new System.Drawing.Size(161, 21);
+            this.cmbPresentacion.TabIndex = 11;
+            this.cmbPresentacion.ValueMember = "Nombre";
+            this.cmbPresentacion.SelectedIndexChanged += new System.EventHandler(this.cmbPresentacion_SelectedIndexChanged);
+            // 
+            // dtpFechaPedido
+            // 
+            this.dtpFechaPedido.Location = new System.Drawing.Point(92, 167);
+            this.dtpFechaPedido.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpFechaPedido.Name = "dtpFechaPedido";
+            this.dtpFechaPedido.Size = new System.Drawing.Size(186, 20);
+            this.dtpFechaPedido.TabIndex = 7;
+            this.dtpFechaPedido.ValueChanged += new System.EventHandler(this.dtpFechaPedido_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 167);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Fecha de Pedido";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 111);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Presentacion";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Producto";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Empresa";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dgvPedidos
+            // 
+            this.dgvPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPedidos.Location = new System.Drawing.Point(11, 224);
+            this.dgvPedidos.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvPedidos.Name = "dgvPedidos";
+            this.dgvPedidos.RowHeadersWidth = 51;
+            this.dgvPedidos.RowTemplate.Height = 24;
+            this.dgvPedidos.Size = new System.Drawing.Size(574, 223);
+            this.dgvPedidos.TabIndex = 11;
+            this.dgvPedidos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPedidos_CellContentClick);
+            // 
+            // FormDatosPedido
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 452);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvPedidos);
+            this.Name = "FormDatosPedido";
+            this.Text = "FormDatosPedido";
+            this.Load += new System.EventHandler(this.FormDatosPedido_Load_1);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPedidos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnFiltrar;
+        private System.Windows.Forms.Button btnCancelarPedido;
+        private System.Windows.Forms.Button btnRegistrarPedido;
+        private System.Windows.Forms.DateTimePicker dtpFiltroFecha;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cmbEmpresa;
+        private System.Windows.Forms.TextBox txtPedidoID;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbProducto;
+        private System.Windows.Forms.ComboBox cmbPresentacion;
+        private System.Windows.Forms.DateTimePicker dtpFechaPedido;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvPedidos;
+    }
+}
---
+++ FormDatosPedido.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoYambolyFinal
+{
+    public partial class FormDatosPedido : Form
+    {
+        public FormDatosPedido()
+        {
+            InitializeComponent();
+        }
+        private void FormDatosPedido_Load(object sender, EventArgs e)
+        {
+            CargarCombos();
+            dtpFechaPedido.Value = DateTime.Today;
+            dtpFiltroFecha.Value = DateTime.Today;
+            ListarPedidosPorFecha(dtpFiltroFecha.Value);
+        }
+        private void CargarCombos()
+        {
+            cmbEmpresa.DataSource = logEmpresa.Instancia.ListarEmpresa();
+            cmbEmpresa.DisplayMember = "Nombre";
+            cmbEmpresa.ValueMember = "EmpresaID";
+
+            cmbProducto.DataSource = logProducto.Instancia.ListarProducto();
+            cmbProducto.DisplayMember = "Producto";
+            cmbProducto.ValueMember = "ProductoID";
+
+            ActualizarComboPresentaciones();
+        }
+        private void txtPedidoID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbEmpresa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrarPedido_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCantidad.Text) || !int.TryParse(txtCantidad.Text, out int cantidad))
+            {
+                MessageBox.Show("Ingrese una cantidad válida.");
+                return;
+            }
+
+            entPedido p = new entPedido
+            {
+                EmpresaID = Convert.ToInt32(cmbEmpresa.SelectedValue),
+                ProductoID = Convert.ToInt32(cmbProducto.SelectedValue),
+                PresentacionID = Convert.ToInt32(cmbPresentacion.SelectedValue),
+                Cantidad = cantidad,
+                FechaPedido = dtpFechaPedido.Value
+            };
+
+            bool registrado = logPedido.Instancia.RegistrarPedido(p);
+            if (registrado)
+            {
+                MessageBox.Show("Pedido registrado correctamente.", "Éxito");
+                ListarPedidosPorFecha(dtpFiltroFecha.Value);
+                LimpiarFormulario();
+            }
+            else
+            {
+                MessageBox.Show("No se pudo registrar el pedido.", "Error");
+            }
+        }
+
+        private void btnCancelarPedido_Click(object sender, EventArgs e)
+        {
+            if (dgvPedidos.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Seleccione un pedido para anular.");
+                return;
+            }
+
+            int id = Convert.ToInt32(dgvPedidos.SelectedRows[0].Cells["PedidoID"].Value);
+            logPedido.Instancia.AnularPedido(id);
+            MessageBox.Show("Pedido anulado correctamente.", "Anulado");
+            ListarPedidosPorFecha(dtpFiltroFecha.Value);
+        }
+        private void ListarTodosLosPedidos()
+        {
+            dgvPedidos.DataSource = logPedido.Instancia.ListarPedidos();
+        }
+        private void ActualizarComboPresentaciones()
+        {
+            if (cmbProducto.SelectedValue != null && cmbProducto.SelectedValue is int productoID)
+            {
+                cmbPresentacion.DataSource = logPresentacion.Instancia.ListarPresentaciones(productoID);
+                cmbPresentacion.DisplayMember = "Presentacion";
+                cmbPresentacion.ValueMember = "PresentacionID";
+            }
+        }
+        private void LimpiarFormulario()
+        {
+            txtCantidad.Clear();
+            cmbEmpresa.SelectedIndex = 0;
+            cmbProducto.SelectedIndex = 0;
+            if (cmbPresentacion.Items.Count > 0)
+                cmbPresentacion.SelectedIndex = 0;
+        }
+        private void ListarPedidosPorFecha(DateTime fecha)
+        {
+            dgvPedidos.DataSource = logPedido.Instancia.ListarPedidosPorFecha(fecha);
+        }
+        private void dtpFechaPedido_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvPedidos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnFiltrar_Click(object sender, EventArgs e)
+        {
+            ListarPedidosPorFecha(dtpFiltroFecha.Value);
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            ListarTodosLosPedidos();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbProducto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbPresentacion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCantidad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtpFiltroFecha_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormDatosPedido_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
