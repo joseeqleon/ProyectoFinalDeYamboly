8e772b6f8c008c2755e1559773b2a015c5130f56
Jose <Jose@DESKTOP-OEDD3GV>
2025-07-09 03:26:56 -0500

    Actualizado

---
+++ CapaEntidad/entVenta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entVenta
+    {
+        public int VentaID { get; set; }
+        public int PedidoID { get; set; }
+        public DateTime FechaVenta { get; set; }
+        public bool Estado { get; set; }
+    }
+}
