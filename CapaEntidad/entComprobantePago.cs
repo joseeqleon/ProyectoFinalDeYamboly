ab8fce274abb325f682a1d2621f036ba7426a335
Jose <Jose@DESKTOP-OEDD3GV>
2025-07-09 02:38:53 -0500

    Se agrego Capa Entidad

---
+++ CapaEntidad/entComprobantePago.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entComprobantePago
+    {
+        public int ComprobanteID { get; set; }
+        public int PedidoID { get; set; }
+        public int MetodoPagoID { get; set; }
+        public DateTime FechaEntrega { get; set; }
+        public bool Estado { get; set; }
+    }
+}
