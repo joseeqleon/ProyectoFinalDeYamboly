60e83896dcb8022a53c4909613951c058d8d201f
Jose <Jose@DESKTOP-OEDD3GV>
2025-07-09 02:59:22 -0500

    Se agrega entPedido

---
+++ CapaEntidad/entPedido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entPedido
+    {
+        public int PedidoID { get; set; }
+        public int EmpresaID { get; set; }
+        public string EmpresaNombre { get; set; }
+        public int ProductoID { get; set; }
+        public int PresentacionID { get; set; }
+        public int Cantidad { get; set; }
+        public DateTime FechaPedido { get; set; }
+        public bool Estado { get; set; }
+    }
+}
