d834b0a704a8f2414e7e4611dfb02cb92efe7fbc
Jose <Jose@DESKTOP-OEDD3GV>
2025-07-09 03:16:17 -0500

    Se agrega entPresentacion

---
+++ CapaEntidad/entPresentacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entPresentacion
+    {
+        public int PresentacionID { get; set; }
+        public int ProductoID { get; set; }
+        public string Presentacion { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+        public bool Estado { get; set; }
+    }
+}
