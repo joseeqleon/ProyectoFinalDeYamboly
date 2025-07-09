d222cea5fa646dcfb671a6b7d19111b8f5f4b77c
Jose <Jose@DESKTOP-OEDD3GV>
2025-07-09 03:26:23 -0500

    Se agrega entUbigeo

---
+++ CapaEntidad/entUbigeo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entUbigeo
+    {
+        public string UbigeoID { get; set; }
+        public string Departamento { get; set; }
+        public string Provincia { get; set; }
+        public string Distrito { get; set; }
+        public override string ToString()
+        {
+            return $"{Departamento} / {Provincia} / {Distrito}";
+        }
+    }
+}
